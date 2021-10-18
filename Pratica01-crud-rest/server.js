99e45c8efe183195d58cdafceeb7c2480c03f766
David Monteiro <davidmonteiro48@gmail.com>
2021-10-18 20:04:36 +0100

    update

--- Pratica01-crud-rest/server.js
+++ Pratica01-crud-rest/server.js
@@ -4,11 +4,12 @@ const app = express(); //construtor que incializa uma aplicação Express
 
 app.use = (express.json()); // Faz o parse (validação e interpretação) de solicitações do tipo application/json
 
-//app.use(express.urlencoded({extended:true})); 
+app.use(express.urlencoded({ extended: true})); 
 //Faz o parse do conteúdo tipo application/x-www-form-urlencoded
 
-app.get("/", (req, res)=> { //cria uma "rota" simples com o verbo GET do HTTP
-    res.json({mensagem: "Bem-Vindo à primeira aplicação CRUD-REST"});
+app.get("/", (req, res) => { //cria uma "rota" simples com o verbo GET do HTTP
+    res.json({
+        mensagem: "Bem-Vindo à primeira aplicação CRUD-REST"})
 });
 
 require("./rotas/crudrest.rotas")(app);
