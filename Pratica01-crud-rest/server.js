8e32e371aca4650f9a8e63931ae3cd04bcc1be4d
David Monteiro <davidmonteiro48@gmail.com>
2021-10-25 02:21:14 +0100

    update

--- Pratica01-crud-rest/server.js
+++ Pratica01-crud-rest/server.js
@@ -1,21 +1,19 @@
-const express =  require("express"); //Carrega o framework express
+const express = require("express"); // Carrega o framework Express
 
-const app = express(); //construtor que incializa uma aplicação Express
+const app = express(); // Construtor que inicializa uma aplicação Express
 
-app.use = (express.json()); // Faz o parse (validação e interpretação) de solicitações do tipo application/json
+app.use(express.json()); // Faz o parse (validação e interpretação) de solicitações do tipo application/json
 
-app.use(express.urlencoded({ extended: true})); 
-//Faz o parse do conteúdo tipo application/x-www-form-urlencoded
+app.use(express.urlencoded({extend: true})); // Faz o parse do conteúdo tipo application/x-www-form-urlenconded
 
-app.get("/", (req, res) => { //cria uma "rota" simples com o verbo GET do HTTP
-    res.json({
-        mensagem: "Bem-Vindo à primeira aplicação CRUD-REST"})
+app.get("/",(req,res) => { // Cria uma "rota" simples com o verbo GET do HTTP
+    res.json({mensagem: "Bem_vindo à primeira apliacação CRUD-REST"})
 });
 
 require("./rotas/crudrest.rotas")(app);
 
-const PORTA = process.env.PORT || 8080 //Estabelece a porta do servidor 
+const PORTA = process.env.PORT || 8081; // Estabelece a porta do servidor
 
 app.listen(PORTA, () => {
-    console.log(`O servidor está a ouvir na porta ${PORTA}`);
+    console.log(`O servidor está a ouvir na porta ${PORTA}`)
 });
