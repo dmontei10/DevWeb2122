a40307f4d61f01018add35fe2755ee7ab0314f4a
David Monteiro <davidmonteiro48@gmail.com>
2021-11-08 14:07:29 +0000

    update

---
+++ Pratica03/public/js/demandHtmlContent.js
@@ -0,0 +1,5 @@
+function getContentHtml(){
+    var iframe = document.getElementById("iFrame");
+    var content = document.getElement.innerHTML;
+    iframe.contentDocument.body.outerText = content;
+}
